cc17404504c28849bcbfda0beb79439c3d4f1a01
JailtonJunior14 <jailtonjrbr@gmail.com>
2024-03-26 11:07:09 -0300

    Adicionar arquivos de projeto.

---
+++ Inteiros1a100/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inteiros1a100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Impar(1), Par(0)");
+            int opcao = int.Parse(Console.ReadLine());
+
+            int cont = opcao;
+
+            while (cont <= 20)
+            {
+                Console.WriteLine(cont);
+                cont+=2;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
